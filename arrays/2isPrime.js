8b37b3f62eb2dc5056c6e6c063d48281f447711c
Chris <cjthornburg@thornburgsfamily.com>
2023-11-26 13:30:38 -0500

    is prime

---
+++ arrays/2isPrime.js
@@ -0,0 +1,12 @@
+const isPrime = (n) => {
+    if (n===1) return false
+    for (let i = n-1; i >1; i--){
+      if (n%i === 0) return false
+    }
+
+    return true
+  };
+
+  module.exports = {
+    isPrime,
+  };
