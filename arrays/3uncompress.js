8b7572762d1998f391ff54c7348a814298603349
Cjthornburg <Cjthornburg@thornburgsfamily.com>
2023-12-03 20:13:36 -0500

    spelling

--- arrays/3uncompress.js
+++ arrays/3uncompress.js
@@ -6,17 +6,17 @@ const uncompress = (s) => {
     while (j< s.length) {
   //     check if current char is number without converting it
       if(numbers.includes(s[j])) {
-  //       if it is increment up on to check the next charcter beause numbers can bet multiple digits
+  //       if it is increment up on to check the next character because numbers can bet multiple digits
         j++;
       }else {
-  //       if in this block we know its the charcter that needs repeating, and number is inbtween current j and current i so slice that out
-  //       and save as number to know how many times the charcter on j is needed
+  //       if in this block we know its the character that needs repeating, and number is in between current j and current i so slice that out
+  //       and save as number to know how many times the character on j is needed
        const num = Number(s.slice(i,j));
         for(let count =0; count <num; count++){
   //         add to base string
           baseString.push(s[j])
         }
-  //       still in else so afte adding charcters need to increment J first so i is at the start of the next number
+  //       still in else so after adding characters need to increment J first so i is at the start of the next number
         j +=1;
         i =j;
       }
