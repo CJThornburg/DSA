e84d47c98b0cc6d0798e15f49ebc5f49c486f7d6
Chris Thornburg <59716885+CJThornburg@users.noreply.github.com>
2023-12-10 20:03:11 -0500

    Add files via upload

---
+++ 2sumList.js
@@ -0,0 +1,21 @@
+// Write a function, sumList, that takes in the head of a linked list containing numbers as an argument. The function should return the total sum of all values in the linked list.
+
+
+const sumList = (head) => {
+
+    if (head === null) return 0
+    return  head.val + sumList(head.next)
+  };
+
+
+  //! iterative
+  // const sumList = (head) => {
+  //   let sum = 0;
+  //   let current = head;
+  //   while (current !== null){
+  //     sum += current.val;
+  //     current = current.next
+
+  //   }
+  //   return sum
+  // }
